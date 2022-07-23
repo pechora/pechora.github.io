c1d907d2c03892085d646eef2b5e087b956ab54e
Adarsh Verma <verma.adarsh96@gmail.com>
2022-07-23 03:15:53 -0400

    V1

---
+++ script.js
@@ -0,0 +1,87 @@
+const plaintextElement = document.getElementById('plaintext');
+const ciphertextElement = document.getElementById('ciphertext');
+const keyElement = document.getElementById('key');
+
+var plaintextEdited = false;
+
+var charArray = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split('');
+charArray = charArray.concat([' ', '.', ',', '!', '?', ':', ';', '"', '\'', '-', '_', '+', '=', '%', '$', '#', '@', '&', '\\', '(', ')']);
+
+Number.prototype.mod = function (n) {
+    "use strict";
+    return ((this % n) + n) % n;
+  };
+
+const plainTextChangeHandler = () => {
+    var plaintext = plaintextElement.value.trim();
+    var key = keyElement.value.trim();
+
+    if (plaintext === '' || key === '') {
+        ciphertextElement.value = '';
+    } else {
+        ciphertextElement.value = encrypt(plaintext, key);
+    }
+
+    plaintextEdited = true;
+}
+
+const cypherTextChangeHandler = () => {
+    var cypherText = ciphertextElement.value.trim();
+    var key = keyElement.value.trim();
+
+    if (cypherText === '' || key === '') {
+        plaintextElement.value = '';
+    } else {
+        plaintextElement.value = decrypt(cypherText, key);
+    }
+
+    plaintextEdited = false;
+}
+
+const keyChangeHandler = () => {
+    var plaintext = plaintextElement.value.trim();
+    var cypherText = ciphertextElement.value.trim();
+    var key = keyElement.value.trim();
+
+    if (plaintext !== '' && plaintextEdited) {
+        ciphertextElement.value = encrypt(plaintext, key);
+    } else if (cypherText !== '' && !plaintextEdited) {
+        plaintextElement.value = decrypt(cypherText, key);
+    }
+}
+
+const encrypt = (plaintext, key) => {
+    var result = plaintext.split('');
+    console.log(charArray);
+
+    result.forEach((element, index, array) => {
+        var lookupIndex = charArray.indexOf(element);
+
+        if (lookupIndex !== -1) {
+            console.log("ENC: lookupIndex of " + element + ":" + lookupIndex + " and key.charCodeAt(" + index % key.length + "):" + key.charCodeAt(index % key.length));
+            array[index] = charArray[(lookupIndex + key.charCodeAt(index % key.length)) % charArray.length];
+        } else {
+            console.log("lookupIndex of " + element + " not found");
+            array[index] = '^';
+        }
+    });
+
+    return result.join('');
+}
+
+const decrypt = (cypherText, key) => {
+    var result = cypherText.split('');
+
+    result.forEach((element, index, array) => {
+        var lookupIndex = charArray.indexOf(element);
+
+        if (lookupIndex !== -1) {
+            console.log("DEC: lookupIndex of " + element + ":" + lookupIndex + " and key.charCodeAt(" + index % key.length + "):" + key.charCodeAt(index % key.length));
+            array[index] = charArray[(lookupIndex - key.charCodeAt(index % key.length)).mod(charArray.length)];
+        } else {
+            array[index] = '^';
+        }
+    });
+
+    return result.join('');
+}
